dbf6e9776dfe508b378b59583a7809f7b7d5b919
YLos <YLos@its.jnj.com>
2024-04-04 01:43:43 +0300

    stage1-module4-collections-task1-list finished

--- src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
+++ src/main/java/com/epam/mjc/collections/list/ArrayListCreator.java
@@ -2,8 +2,17 @@ package com.epam.mjc.collections.list;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 public class ArrayListCreator {
     public ArrayList<String> createArrayList(List<String> sourceList) {
+        List<String> result = new ArrayList<>();
+        for (int i = 2; i < sourceList.size(); i += 3) {
+            result.add(sourceList.get(i));
+            result.add(sourceList.get(i));
+        }
+        return (ArrayList<String>) result;
+
     }
 }
+
--- src/main/java/com/epam/mjc/collections/list/ListSorter.java
+++ src/main/java/com/epam/mjc/collections/list/ListSorter.java
@@ -1,15 +1,29 @@
 package com.epam.mjc.collections.list;
 
+import java.util.Collections;
 import java.util.Comparator;
 import java.util.List;
 
 public class ListSorter {
     public void sort(List<String> sourceList) {
+        Collections.sort(sourceList, new ListComparator());
     }
 }
 
 class ListComparator implements Comparator<String> {
     @Override
     public int compare(String a, String b) {
+        double stringA =  (5 * Math.pow(Integer.parseInt(a), 2) + 3);
+        double stringB =  (5 * Math.pow(Integer.parseInt(b), 2) + 3);
+
+        if (stringA == stringB){
+
+            return Integer.valueOf(a).compareTo(Integer.valueOf(b));
+        }else if (stringA > stringB){
+            return 1;
+        }else{
+            return -1;
+        }
+
     }
 }
